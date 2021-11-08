e10352c63a8f9083af2c5ff8c956dc03958dc80b
Erdinç <9353938+erdincsk@users.noreply.github.com>
2021-11-08 12:04:42 +0300

    Update join-setup-ceremony.sh

--- join-setup-ceremony.sh
+++ join-setup-ceremony.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+#mutlaka yeni bir screen'de işlemleri yapın. !!!!
+
+screen -S aleo-c
 cd
 sudo apt-get install libssl-dev -y
 sudo apt-get install pkg-config -y
@@ -12,7 +16,11 @@ cd ~/aleo-setup
 cargo clean && cargo build --release
 cargo run --release --bin setup1-contributor -- generate --keys-path keys.json
 #şifre tanımla
-screen -S aleo-c
 cargo run --release --bin setup1-contributor -- contribute --api-url https://inner-ceremony.aleo.org --keys-path keys.json
 #tanımladıgın şifreyi gir
+#sizi sıraya alacak kaçıncı sırada oldugunuz gözükecek
+
+
+
+#sıra bittiğinde eth adresinizi gireceksiniz ve belli bir süre sonra nft o eth adresine gelecek
 
