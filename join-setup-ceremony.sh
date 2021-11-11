8f58abe0670ba65496f6762d1372d94476e407aa
Erdinç <9353938+erdincsk@users.noreply.github.com>
2021-11-11 10:22:48 +0300

    Update join-setup-ceremony.sh

--- join-setup-ceremony.sh
+++ join-setup-ceremony.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
 cd
-sudo apt-get install libssl-dev pkg-config libclang-dev -y
+sudo apt-get install libssl-dev -y && sudo apt-get install pkg-config -y && sudo apt-get install libclang-dev -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
 #gelen pencerede 1'e bas
-
-#kurulum sonrası terminali yeniden başlat ya da yeni terminal aç. ardından aşağıdaki git clone komutu ile devam et.
-
+#kurulum sonrası terminali yeniden başlat
 git clone https://github.com/aleoHQ/aleo-setup
 cd ~/aleo-setup && cargo clean && cargo build --release
 cargo run --release --bin setup1-contributor -- generate --keys-path keys.json
 #şifre tanımla
-
-#bundan sonraki işlemleri mutlaka yeni bir screen'de yapın. !!!!
-
 screen -S aleo-c
-cd ~/aleo-setup
 cargo run --release --bin setup1-contributor -- contribute --api-url https://inner-ceremony.aleo.org --keys-path keys.json
 #tanımladıgın şifreyi gir
-#sizi sıraya alacak kaçıncı sırada oldugunuz gözükecek
-
-
-#sıra bittiğinde eth adresinizi gireceksiniz ve belli bir süre sonra nft o eth adresine gelecek
 
